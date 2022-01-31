c533c013957e3340db368a124049c75e870a7d9f
angelo-urtula <aurtula1@gmail.com>
2022-01-31 07:48:07 -0500

    post controller set up

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+    def index
+        posts = Post.all
+        render json: post.to_json(include: [:user])
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            render json: @post, status: : created
+        else
+            render json: @post.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:user_id, :title, :content)
+    end
 end
