3b34b1a7f5f20ebfb92c296c08b25fd1cc2815b7
angelo-urtula <aurtula1@gmail.com>
2022-01-31 09:49:10 -0500

    attempting to set up redux thunk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import {createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+import combinedReducers from "./reducers/combinedReducers";
+
+const store = createStore(combinedReducers, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
