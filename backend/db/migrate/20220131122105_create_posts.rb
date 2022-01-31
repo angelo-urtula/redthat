4cbe6bb131fd965c3e3dd316206c5dbf9db63ec8
angelo-urtula <aurtula1@gmail.com>
2022-01-31 07:31:18 -0500

    post will reference user

--- backend/db/migrate/20220131122105_create_posts.rb
+++ backend/db/migrate/20220131122105_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
+      t.references :user, null: false, foreign_key: { on_delete: :cascade }
       t.string :title
       t.string :content
 
