8e7410662f2499fcc249d43ba1845e1dada92978
angelo-urtula <aurtula1@gmail.com>
2022-01-31 08:17:58 -0500

    corrected typos

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     def index
         users = User.all
-        render json: user.to_json(include: [:posts])
+        render json: users.to_json(include: [:posts])
     end
 
     def show
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            render json: @user, status :created
+            render json: @user, status: :created
         else
             render json: @user.errors, status: :unprocessable_entity
         end
