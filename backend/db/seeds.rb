1cc964bc0d873924e453c1071aa3ab446593dc5b
angelo-urtula <aurtula1@gmail.com>
2022-01-31 08:18:17 -0500

    added seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create(:name => "Angelo", :email => "random@yahoo.com", :hobbies => "watch tv")
+
+Post.create(:user_id => 3, :title => "I like to eat", :content => "Hi, my name is Angelo and I like to eat food")
