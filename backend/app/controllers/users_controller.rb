16c0a5041c4c5de3c568ec83c2704d4ddf88e403
angelo-urtula <aurtula1@gmail.com>
2022-01-31 07:35:07 -0500

    index and show for user

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: user.to_json(include: [:posts])
+    end
+
+    def show
+        render json: @user
+    end
 end
