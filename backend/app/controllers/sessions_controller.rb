ea784ef139f0070ef3ec68352e813b9eff970caa
angelo-urtula <aurtula1@gmail.com>
2022-02-08 10:07:08 -0500

    attempting to figure out sessions not persisting

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,8 +1,13 @@
 class SessionsController < ApplicationController
     def create
-        user = User.find_by(email: params[:email])
-        session[:user_id] = user.id
-        render json: user
+        user = User.find_by(:email => params[:email])
+        if user
+            session[:user_id] = user.id
+            cookies[:user_id] = user.id
+            render json: user
+        else
+            flash.now[:danger] = "Incorrect email and/or password"
+        end
     end
 
     def destroy
